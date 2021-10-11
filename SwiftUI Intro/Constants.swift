278c83e6c8688468fc2a10f51384e4340a54c3ca
Daniel Hu <hdmdhr@gmail.com>
2021-10-11 17:12:48 -0600

    Add Constants file.

---
+++ SwiftUI Intro/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  SwiftUI Introduction
+//
+//  Created by 胡洞明 on 2021-10-11.
+//
+
+import Foundation
+
+enum Constants {
+    
+    static let placeApiBaseUrlString = "https://sample-api.azure-api.net/sample/places/"
+    
+    static let stagingApiBaseUrlString = "https://api.staging.nudge.vogdevelopment.com/api/"
+    
+}
