6b945a677ffa79109567e53d7c30522568d9238c
Daniel Hu <hdmdhr@gmail.com>
2021-10-16 10:48:28 -0600

    Add protocol `AQueryItemProvider`, make String keyed dictionary conform to the protocol.

---
+++ SwiftUI Intro/Data/Network/AQueryItemProvider.swift
@@ -0,0 +1,25 @@
+//
+//  AQueryItemProvider.swift
+//  SwiftUI Intro
+//
+//  Created by 胡洞明 on 2021-10-16.
+//
+
+import Foundation
+
+protocol AQueryItemProvider {
+    
+    func queryItems(jsonEncoder: JSONEncoder) throws -> [URLQueryItem]?
+    
+}
+
+
+extension Dictionary: AQueryItemProvider where Key == String {
+    
+    func queryItems(jsonEncoder: JSONEncoder = .init()) -> [URLQueryItem]? {
+        reduce(into: [URLQueryItem]()) { acc, keyValue in
+            acc?.append(URLQueryItem(name: keyValue.key, value: "\(keyValue.value)"))
+        }
+    }
+    
+}
