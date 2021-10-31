fca79071508b080109bec1c11bb2cc91843cf064
Daniel Hu <hdmdhr@gmail.com>
2021-10-31 16:08:29 -0600

    Add URL+UrlConvertible extension

---
+++ SwiftUI Intro/Data/Network/Ext/URL+UrlConvertible.swift
@@ -0,0 +1,14 @@
+//
+//  URL+UrlConvertible.swift
+//  SwiftUI Intro
+//
+//  Created by 胡洞明 on 2021-10-31.
+//
+
+import Foundation
+
+extension URL: UrlConvertible {
+    
+    var optionalUrl: URL? { self }
+    
+}
