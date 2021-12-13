c714d6313575e5239b3dd344999c07086072e7fb
Daniel Hu <hdmdhr@gmail.com>
2021-12-13 11:49:42 -0700

    Remove combine http client, use AsyncHttpClient in ContentView

--- SwiftUI Intro/ContentView.swift
+++ SwiftUI Intro/ContentView.swift
@@ -6,41 +6,25 @@
 //
 
 import SwiftUI
-import CoreData
-import Combine
 
 struct ContentView: View {
 
-    @State private var bag = Set<AnyCancellable>()
     @State private var places: [Places.Place] = []
     
     var body: some View {
         List(places, id: \.id) { place in
             Text("\(place.name)\n\(place.address)")
         }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
+        .task {
+            let httpClient = AsyncHttpClient(urlSession: .shared)
+            
+            guard let response: PagingEnvelop<Places.Place> = try? await httpClient.request(
+                url: APIs.Places.searchPlaces,
+                method: .get(queryItemsProvider: Places.SearchQuery()))
+            else { return }
+            
+            self.places = response.data.items
         }
-        .onAppear(perform: {
-            let httpClient = EndpointHttpClient(urlSession: .shared, endpointType: APIs.Places.self)
-            httpClient.networkRequestPublisher(endpoint: .searchPlaces,
-                                               method: .get(queryItemsProvider: Places.SearchQuery()))
-                .map{ (env: PagingEnvelop<Places.Place>) in
-                    env.data.items
-                }
-                .sink(receiveCompletion: { completion in
-                    switch completion {
-                    case .failure(let networkError):
-                        print(networkError.description)
-                    case .finished: break
-                    }
-                }, receiveValue: { places in
-                    self.places = places
-                })
-                .store(in: &bag)
-        })
     }
 
 }
@@ -48,6 +32,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
+        ContentView()
     }
 }
